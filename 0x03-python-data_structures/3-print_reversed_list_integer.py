35c341251e10f64d04f864dd2be9e12c49484d74
Dasurf <damilareyusuf.dy@gmail.com>
2024-01-06 21:17:39 +0100

    Python higher level programming

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,6 @@
 
 if __name__ == "__main__":
 
-    thisList = [6, 7, 2, 8]
-
     def print_reversed_list_integer(my_list=[]):
         my_list.reverse()
         return my_list
-    print(print_reversed_list_integer(thisList))
