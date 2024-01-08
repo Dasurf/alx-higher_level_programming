f00a1a47e13ec5929702ac6203447038f126f9a3
Dasurf <damilareyusuf.dy@gmail.com>
2024-01-08 23:29:24 +0100

    Python higher level programming

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,4 +4,6 @@ def multiple_returns(sentence):
     tolist = list(sentence)
     tostr = ''.join(map(str, tolist))
     strlen = len(tostr)
+    if strlen == 0:
+        return 0, "None"
     return strlen, tostr[0]
