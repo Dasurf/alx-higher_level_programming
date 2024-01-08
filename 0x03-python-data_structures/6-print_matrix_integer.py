d0e29e22d2bb73d405d5779d68a329f37af310e6
Dasurf <damilareyusuf.dy@gmail.com>
2024-01-08 18:22:35 +0100

    Python higher level programming

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
-
-matrix_no = [
-    [3, 4, 5, 6],
-    [12, 45, 67, 89],
-    [1, 2, 7, 9]
-]
-
 def print_matrix_integer(matrix=[[]]):
-    my_string = ''
-    for i in matrix:
-        my_string = ', '.join(map(str, i))
-        print("{}".format(my_string))
-    return my_string
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
