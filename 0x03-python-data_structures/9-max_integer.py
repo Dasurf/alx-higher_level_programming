e4a5ec1847c1ad052b956aa376ec1e650aa47011
Dasurf <damilareyusuf.dy@gmail.com>
2024-01-08 23:42:00 +0100

    Python higher level programming

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,10 @@
 
 def max_integer(my_list=[]):
     temp = 0
-
-    for i in my_list:
-        if i > temp:
-            temp = i
-    return temp
+    if my_list:
+        for i in my_list:
+            if i > temp:
+                temp = i
+        return temp
+    else:
+        return None
