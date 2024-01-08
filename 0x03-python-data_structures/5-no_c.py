0034e2452c6be5e2ed7faebd85886bc45854adae
Dasurf <damilareyusuf.dy@gmail.com>
2024-01-08 18:37:34 +0100

    Python higher level programming

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,5 +5,5 @@ def no_c(my_string):
     for i in arr:
         if i == 'c' or i == 'C':
             arr.remove(i)
-            my_string = ''.join(map(str, arr))
-    return my_string
+            new_string = ''.join(map(str, arr))
+    return new_string
