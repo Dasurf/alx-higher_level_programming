71c9cf7952f965226dc73b41184fb88e15d32d76
Dasurf <damilareyusuf.dy@gmail.com>
2024-01-09 18:33:52 +0100

    Python higher level programming

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
 
-    if  (len(sys.argv) - 1) != 3:
+    if (len(sys.argv) - 1) != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if (sys.argv[2] not in list(ops.keys())):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
