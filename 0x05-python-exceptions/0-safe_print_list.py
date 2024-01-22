96957f268bb495c78cbdaab6f4858c48d71b37f2
Dasurf <damilareyusuf.dy@gmail.com>
2024-01-22 15:59:48 +0100

    Python higher level programming

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    if x:
-        try:
-            length = 0
-            for item in my_list:
-                length += 1
-            if x > length:
-                x = length
-            for i in range(x):
-                print(my_list[i], end="")
-            print()
-            return (x)
-        except ValueError as e:
-            print("Invalid value input", e)
-        except TypeError as e:
-            print("Incompatible type of data")
-        except Exception as e:
-            print("Something went wrong", e)
-    else:
-        print("Argument value not found")
+        count = 0
+        for i in range(x):
+                try:
+                        print("{}".format(my_list[i]), end="")
+                        count = count + 1
+                except IndexError:
+                        break
+        print("")
+        return count
