1ff4d1db5d8cbe0ba078d3ed08cb66c4df52660f
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-09 23:03:35 +0100

    another fix in advanced task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const totalist = Object.entries(dict);
+const total_list = Object.entries(dict);
 const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
 for (const j in valsUniq) {
   const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
+  for (const k in total_list) {
+    if (total_list[k][1] === valsUniq[j]) {
+      list.push(total_list[k][0]);
     }
   }
   newDict[valsUniq[j]] = list;
