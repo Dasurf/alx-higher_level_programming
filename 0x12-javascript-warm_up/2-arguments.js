45e7a4826d3ac3c75b7ed36b5f8f0e3543c285c0
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-10 00:48:35 +0100

    semistandard fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const fs = require('fs');
 if (process.argv.length < 3) {
-    console.log("No argument");
-}
-else if (process.argv.length === 3) {
-    console.log("Argument found");
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-    console.log("Arguments found");
+  console.log('Arguments found');
 }
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs');
-
 if (!process.argv[2]) {
-    console.log("No argument");
+  console.log('No argument');
 } else {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 }
