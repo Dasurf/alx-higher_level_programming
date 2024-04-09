e6b4b2300059cab6f22872c1c0942250a08ec810
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-09 23:06:27 +0100

    replaced snake case to camel case to meet semistandard

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const total_list = Object.entries(dict);
+const totalLlist = Object.entries(dict);
 const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
 for (const j in valsUniq) {
   const list = [];
-  for (const k in total_list) {
-    if (total_list[k][1] === valsUniq[j]) {
-      list.push(total_list[k][0]);
+  for (const k in totalLlist) {
+    if (totalLlist[k][1] === valsUniq[j]) {
+      list.push(totalLlist[k][0]);
     }
   }
   newDict[valsUniq[j]] = list;
