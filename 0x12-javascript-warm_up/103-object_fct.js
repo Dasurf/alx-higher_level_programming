7568d37b4e9602febed8721541acfe64c48d3d50
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-10 15:05:38 +0100

    final fix of task 103 to give correct output

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -5,7 +5,10 @@ const myObject = {
 };
 console.log(myObject);
 
-myObject.incr = () => this.value++;
+myObject.incr = function () 
+{
+    this.value++;
+}
 
 myObject.incr();
 console.log(myObject);
