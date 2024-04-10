e80fa0f011682286e1a9a29f6c14d278338a813c
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-10 14:50:19 +0100

    semistandard fix

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 exports.addMeMaybe = function (n, incrFn) {
-    n++;
-    incrFn(n);
-}
+  n++;
+  incrFn(n);
+};
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 // creating a function that is visible from other files
-exports.add = function add(a, b) {
+exports.add = function add (a, b) {
   return a + b;
-}
+};
 
 const add = require('13-add.js');
 console.log(add(3, 7));
