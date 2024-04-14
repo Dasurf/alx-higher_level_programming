5591aee81cbd84c7d1afaa809d08de85468b654d
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-14 06:52:05 +0100

    task 0 reverted

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-""" Scripts lists all states from the database hbtn_0e_0_usa  and
-takes in 3 arguments: mysql username, mysql password and database name
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
 """
 
-if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+import MySQLdb
+from sys import argv
 
+if __name__ == '__main__':
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
