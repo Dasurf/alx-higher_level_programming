127f6e03670462c2c5b876cb38c189dc03f5ac6e
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-25 14:26:22 +0100

    another tweak to task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to the URL, displays size of the response
-curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
+# Get the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
