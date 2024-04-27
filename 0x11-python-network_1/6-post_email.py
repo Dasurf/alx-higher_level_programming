6ec61a8f48c2406d5c20591ad6149dbeb99b723e
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-27 03:53:18 +0100

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import requests
+import sys
+
+# Retrieve URL and email address from command-line arguments
+url = sys.argv[1]
+email = sys.argv[2]
+
+# Send a POST request to the specified URL with the email as a parameter
+response = requests.post(url, data={'email': email})
+
+# Display the body of the response
+print(response.text)
