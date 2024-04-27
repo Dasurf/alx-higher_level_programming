e09f773e9375f6d7e5b76e230a4cdfdc896f9c6c
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-27 03:51:50 +0100

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+import requests
+import sys
+
+# Retrieve URL from command-line arguments
+url = sys.argv[1]
+
+# Send a GET request to the specified URL
+response = requests.get(url)
+
+# Retrieve the value of the X-Request-Id header from the response
+x_request_id = response.headers.get('X-Request-Id')
+
+# Display the value of the X-Request-Id header
+print(x_request_id)
