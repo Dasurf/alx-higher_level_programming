eb81dad89ca8e17ed9433a31d1f5b09f979271f2
Dasurf <damilareyusuf.dy@gmail.com>
2024-04-27 03:49:01 +0100

    hotfixes

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-
 import urllib.request
 import urllib.error
 import sys
 
-# Retrieve URL from command-line arguments
 url = sys.argv[1]
 
 try:
-    # Make a request to the URL
     with urllib.request.urlopen(url) as response:
-        # Read and decode the response body
         response_body = response.read().decode('utf-8')
-        # Display the body of the response
         print(response_body)
-
 except urllib.error.HTTPError as e:
-    # If an HTTP error occurs, print the error code
     print("Error code:", e.code)
